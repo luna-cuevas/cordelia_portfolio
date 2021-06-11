46882e6398474bcab13439fa4f32d3f36f44e5cc
Luna <s.cuevas14@gmail.com>
2021-06-11 15:54:50 -0700

    updated media queries and added a footer
    updated some stats on product-media-card and ensured responsive design.

--- php/send-email.php
+++ php/send-email.php
@@ -44,6 +44,7 @@ if($_POST) {
 	$headers .= "Reply-To: ". $email . "\r\n";
  	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+   $headers .= "X-Priority: 1\r\n";
 
    ini_set("sendmail_from", $to); // for windows server
    $mail = mail($to, $subject, $message, $headers);
