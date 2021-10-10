6aa9ca0829f2e142c37239cbad6459db9bb66bab
Luna Cuevas <s.cuevas14@gmail.com>
2021-10-10 16:10:36 -0700

    Creating Videos Page
    Routed a second page and populated with some vimeo videos that play on hover. Also included a modal component that opens when clicked.

--- src/app.js
+++ src/app.js
@@ -4,18 +4,32 @@ import Nav from './components/Nav/Nav'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Projects from './components/ProjectSection/Projects'
+import Videos from './Videos/Videos'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/react-modal-video/css/modal-video.css";
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 
 
 
 function App() {
   return (
+    <Router>
     <div>
       <Nav />
-      <Hero />
-      <About />
-      <Projects />
+      <Switch> 
+        <Route exact path='/' >
+          <Hero />
+          <About />
+          <Projects />
+        </Route>
+        <Route exact path='/videos' >
+          <Videos />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
--- src/components/ProjectSection/Projects.jsx
+++ src/components/ProjectSection/Projects.jsx
@@ -2,6 +2,8 @@ import React, { useState, useCallback } from 'react'
 import './ProjectsStyle.css'
 import Carousel from 'react-bootstrap/Carousel'
 import ReactPlayer from 'react-player'
+import { Link } from 'react-router-dom'
+
 
 
 
@@ -11,27 +13,43 @@ const Projects = () => {
 
     return (
         <div className='projects-container'>
-            <div>
-                
-            </div>
-            <div className='projects-box'>  
-                <Carousel id='caro' variant="dark">
-                    <Carousel.Item>
-                        <div className='video'>
-                            <ReactPlayer  height='500px' className="d-block w-100" url='https://vimeo.com/534212643' />
-                        </div>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <div className='video'>
-                            <ReactPlayer  height='500px' className="d-block w-100" url='https://vimeo.com/534212643' />
-                        </div>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <div className='video'>
-                            <ReactPlayer  height='500px' className="d-block w-100" url='https://vimeo.com/534212643' />
-                        </div>
-                    </Carousel.Item>
-                </Carousel>
+            
+            <div className='projects-box'>
+                <div className='projects-text'>
+                    <div className='text1'>
+                        <h1>Hello</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse temporibus magnam aspernatur nobis voluptas nemo soluta excepturi est consequatur laborum.</p>
+                        <Link to='/videos'>
+                        <button>Check out more videos!</button>
+                        </Link>
+                    </div>
+                </div>
+                <div className='projects-video'>
+                    <Carousel width='200px' fade='true' interval='20000' variant="dark">
+                        <Carousel.Item>
+                            <div className='video'>
+                                <ReactPlayer loop playing muted 
+                                height='100%'
+                                width='100%'
+                                className="d-block w-100" url='https://vimeo.com/534212643' />
+                            </div>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <div className='video'>
+                                <ReactPlayer loop muted
+                                height='100%'
+                                width='100%'  className="d-block w-100" url='https://vimeo.com/534212643' />
+                            </div>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <div className='video'>
+                                <ReactPlayer loop muted
+                                height='100%'
+                                width='100%'  className="d-block w-100" url='https://vimeo.com/534212643' />
+                            </div>
+                        </Carousel.Item>
+                    </Carousel>
+                </div>  
             </div>
         </div>
     )
