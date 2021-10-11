27388b22c91c162b10916051760dfa1826ad4ddf
Luna Cuevas <s.cuevas14@gmail.com>
2021-10-10 21:56:10 -0700

    updated tik tok videos page and styled home page.

--- src/app.js
+++ src/app.js
@@ -4,31 +4,28 @@ import Nav from './components/Nav/Nav'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Projects from './components/ProjectSection/Projects'
-import Videos from './Videos/Videos'
+import Videos from './components/Videos/Videos'
+import './components/misc./groovin.ttf'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "../node_modules/react-modal-video/css/modal-video.css";
-
+import "../node_modules/react-modal-video/scss/modal-video.scss";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-
-
-
 
 function App() {
   return (
     <Router>
-    <div>
-      <Nav />
-      <Switch> 
-        <Route exact path='/' >
-          <Hero />
-          <About />
-          <Projects />
-        </Route>
-        <Route exact path='/videos' >
-          <Videos />
-        </Route>
-      </Switch>
-    </div>
+      <div>
+        <Nav />
+        <Switch> 
+          <Route exact path='/' >
+            <Hero />
+            <About />
+            <Projects />
+          </Route>
+          <Route exact path='/videos' >
+            <Videos />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
 import './AboutStyle.css'
 import cordelia from '../img/Cordelia-Headshot.png'
+import sailormoon from '../img/sailormoon.gif'
+
 
 const About = () => {
     return (
         <div className='about-container'>
             <div className='about-box'>
+                {/* <div className='gifs'>
+                    <img src={sailormoon} alt="" />
+                </div> */}
                 <div className='summary'>
                     <img src={cordelia} alt="" />
-                    <h1>About Me</h1>
+                    <h2>About Me</h2>
                     <p>I have been working in the fashion industry for 4 years, starting off as a visual merchandiser at Forever 21 and working my way to becoming a social media associate for Dolls Kill specializing in making video content and Pinterest boards.</p>
                     <p>I have been pursing artistic skating for 5 years with high marks in performance and personality. I have skated for entertainment purposes with Netflix & advertised skates for various skate brands like Sugar Thrillz, C7 skates, and Impala. Skating & fashion are two mediums I use to express myself & I am incredibly lucky to have success in both.</p>
                 </div>
                 <div className='skills'>
-                    <h1>Skills</h1>
+                    <h2>Skills</h2>
                     <div className='skills-box'>
                         <div className='skill'>
-                            <h3>Aesthetics</h3>
+                            <h4>Aesthetics</h4>
                             <p>Using knowledge of different aesthetics to make products look appealing to a wide audience. Whatever your brand is, I got you! I have been an avid Pinterest and tumblr user for 10 years and know exactly how to curate a vision no matter what you’re customers vibe is.</p>
                         </div>
                         <div className='skill'>
-                            <h3>Product Focused</h3>
+                            <h4>Product Focused</h4>
                             <p>This is not about me wanting free clothes or a tag on IG. I am here to make a product look good! My content focuses on showing off profit details and making something looking rad af</p>
                         </div>
                         <div className='skill'>
-                            <h3>Engaging</h3>
+                            <h4>Engaging</h4>
                             <p>This not only means content that makes a someone stop to look at a product but also staying in with latest memes or “inside jokes” of the internet.</p>
                         </div>
                     </div>
